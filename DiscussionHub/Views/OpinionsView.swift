df4c459a2ebdd0adc8940571c3298d48bf71c4da
Yu357 <forestgreen14689@gmail.com>
2022-01-27 20:21:29 +0900

    好きな内容のopinionを追加できるようにした

--- DiscussionHub/Views/OpinionsView.swift
+++ DiscussionHub/Views/OpinionsView.swift
@@ -13,6 +13,8 @@ struct OpinionsView: View {
     let discussionTitle: String
     @ObservedObject var opinionViewModel: OpinionViewModel
     
+    @State var inputStr = ""
+    
     init(discussionId: String, discussionTitle: String) {
         self.discussionId = discussionId
         self.discussionTitle = discussionTitle
@@ -20,26 +22,54 @@ struct OpinionsView: View {
     }
 
     var body: some View {
-        List {
-            ForEach(opinionViewModel.opinions) {opinion in
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("\(opinion.order)")
-                        Text(opinion.speakerId)
-                        Text("\(formatDate(inputDate: opinion.createdAt))")
-                            .foregroundColor(.secondary)
+        
+        VStack {
+            
+            // Opinions list
+            List {
+                ForEach(opinionViewModel.opinions) {opinion in
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("\(opinion.order)")
+                            Text(opinion.speakerId)
+                            Text("\(formatDate(inputDate: opinion.createdAt))")
+                                .foregroundColor(.secondary)
+                        }
+                        Text(opinion.content)
+                            .padding(.top, 2)
                     }
-                    Text(opinion.content)
-                        .padding(.top, 2)
+                    .padding(6)
                 }
-                .padding(6)
+            }
+                .listStyle(PlainListStyle())
+            
+            // New opinion content input area
+            HStack(alignment: .bottom) {
+                TextField("メッセージを入力", text: $inputStr)
+                    .submitLabel(.done)
+                    .font(.body)
+                    .frame(height: 30)
+                    .padding(.vertical, 4)
+                    .padding(.leading, 16)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 19)
+                            .stroke(Color.secondary, lineWidth: 1)
+                    )
+                    .padding(8)
+                
+                Button(action: {
+                    opinionViewModel.addOpinion(content: inputStr)
+                    inputStr = ""
+                }){
+                    Image(systemName: "paperplane.fill")
+                        .font(.title3)
+                        .padding(.trailing)
+                        .padding(.bottom)
+                }
+                    .disabled(inputStr.isEmpty)
             }
         }
-        .listStyle(PlainListStyle())
             .navigationBarTitle(discussionTitle)
-            .navigationBarItems(trailing: Button("plus") {
-                    opinionViewModel.addOpinion(content: "I think so too")
-            })
     }
     
     func formatDate(inputDate: Date) -> String {
