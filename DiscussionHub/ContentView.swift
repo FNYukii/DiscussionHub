a53303839fe40bf4a6a3df5ea62e2843c0525070
Yu357 <forestgreen14689@gmail.com>
2022-01-27 09:50:19 +0900

    Initial commit

---
+++ DiscussionHub/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  DiscussionHub
+//
+//  Created by Yu on 2022/01/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ DiscussionHub/DiscussionHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscussionHubApp.swift
+//  DiscussionHub
+//
+//  Created by Yu on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct DiscussionHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
