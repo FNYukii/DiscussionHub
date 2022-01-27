5d7bb1384789140ff61b72d0b748b3af00041bff
Yu357 <forestgreen14689@gmail.com>
2022-01-27 11:11:57 +0900

    全てのdiscussionドキュメントを取得する処理を記述した

--- DiscussionHub/Views/FirstView.swift
+++ DiscussionHub/Views/FirstView.swift
@@ -6,9 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct FirstView: View {
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear(perform: load)
+    }
+        
+    func load() {
+        
+        // Get all descussion document
+        let db = Firestore.firestore()
+        let docRef = db.collection("discussions")
+        docRef.getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("hello Fail getting document: \(error)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("hello \(document.documentID) : \(document.data())")
+                }
+            }
+        }
     }
 }
