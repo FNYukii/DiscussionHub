9a867f2f26b666ad2bdcaf4243f72e4e15a1d9d4
Yu357 <forestgreen14689@gmail.com>
2022-01-28 11:16:25 +0900

    Tab Barを一旦非表示

--- DiscussionHub/DiscussionHubApp.swift
+++ DiscussionHub/DiscussionHubApp.swift
@@ -13,7 +13,7 @@ struct DiscussionHubApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FirstView()
         }
     }
 }
