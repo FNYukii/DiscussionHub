dbf475452c9fa3f1fda922c1b0b2595ad1a5d002
Yu357 <forestgreen14689@gmail.com>
2022-02-04 00:17:24 +0900

    コメントの投稿日時を、現在との差分で表示するようにした

--- DiscussionHub/Components/AllCommentForeach.swift
+++ DiscussionHub/Components/AllCommentForeach.swift
@@ -26,7 +26,7 @@ struct AllCommentForeach: View {
                     Text("@\(comment.authorDailyId)")
                         .foregroundColor(.secondary)
                         .lineLimit(1)
-                    Text("\(formatDate(inputDate: comment.createdAt))")
+                    Text("\(toHowManyAgo(from: comment.createdAt))")
                         .foregroundColor(.secondary)
                         .lineLimit(1)
                     Spacer()
@@ -45,9 +45,41 @@ struct AllCommentForeach: View {
         }
     }
     
-    func formatDate(inputDate: Date) -> String {
+    private func formatDate(inputDate: Date) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd E HH:mm"
+        dateFormatter.dateFormat = "yyyy-MM-dd E HH:mm:ss"
         return dateFormatter.string(from: inputDate)
+    }
+    
+    private func toHowManyAgo(from: Date) -> String {
+        let inputDate = from
+        
+        let secondDiff: Int = (Calendar.current.dateComponents([.second], from: inputDate, to: Date())).second!
+        if secondDiff < 60 {
+            return "\(secondDiff)秒前"
+        }
+        
+        let minuteDiff = (Calendar.current.dateComponents([.minute], from: inputDate, to: Date())).minute!
+        if minuteDiff < 60 {
+            return "\(minuteDiff)分前"
+        }
+        
+        let hourDiff = (Calendar.current.dateComponents([.hour], from: inputDate, to: Date())).hour!
+        if hourDiff < 24 {
+            return "\(hourDiff)時間前"
+        }
+        
+        let dayDiff = (Calendar.current.dateComponents([.day], from: inputDate, to: Date())).day!
+        if dayDiff < 31 {
+            return "\(dayDiff)日前"
+        }
+        
+        let monthDiff = (Calendar.current.dateComponents([.month], from: inputDate, to: Date())).month!
+        if monthDiff < 12 {
+            return "\(monthDiff)ヶ月前"
+        }
+
+        let yearDiff = (Calendar.current.dateComponents([.year], from: inputDate, to: Date())).year!
+        return "\(yearDiff)年前"
     }
 }
